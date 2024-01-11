9c09a834e1a000bbf2a5fea0de25de7e781add8c
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-01-11 18:04:05 +0100

    css

--- src/App.tsx
+++ src/App.tsx
@@ -9,47 +9,52 @@ function App() {
 	const handleCounter = () => {
 		setNbCounter((prev) => prev + 1);
 	};
-  const counterLoop = () => {
-    const array = [];
-    for (let i =0; i<3;i++){
-        array.push(<Counter
-						show={nbCounter > i ? "block" : "none"}
-						style1={
-							counter[i] === 0 ? { display: "none" } : { display: "block" }
-						}
-						style2={
-							counter[i] === 10 ? { display: "none" } : { display: "block" }
-						}
-						value={counter[i]}
-						clickMoins={() => {
-							setCounter((counter) => {
-								const counter2 = [...counter];
-								counter2[i] = counter[i] - 1;
-								return counter2;
-							});
-						}}
-						clickPlus={() => {
-							setCounter((counter) => {
-								const counter2 = [...counter];
-								counter2[i] = counter[i] + 1;
-								return counter2;
-							});
-						}}
-						clickReset={() => {
-							setCounter((counter) => {
-								const counter2 = [...counter];
-								counter2[i] = 0;
-								return counter2;
-							});
-						}}
-					/>)
-      }
-      return array;
-  }
+	const counterLoop = () => {
+		const array = [];
+		for (let i = 0; i < 3; i++) {
+			array.push(
+				<Counter
+					show={nbCounter > i ? "block" : "none"}
+					style1={counter[i] === 0 ? { display: "none" } : { display: "block" }}
+					style2={
+						counter[i] === 10 ? { display: "none" } : { display: "block" }
+					}
+					value={counter[i]}
+					clickMoins={() => {
+						setCounter((counter) => {
+							const counter2 = [...counter];
+							counter2[i] = counter[i] - 1;
+							return counter2;
+						});
+					}}
+					clickPlus={() => {
+						setCounter((counter) => {
+							const counter2 = [...counter];
+							counter2[i] = counter[i] + 1;
+							return counter2;
+						});
+					}}
+					clickReset={() => {
+						setCounter((counter) => {
+							const counter2 = [...counter];
+							counter2[i] = 0;
+							return counter2;
+						});
+					}}
+				/>
+			);
+		}
+		return array;
+	};
 	return (
 		<>
-			<button onClick={handleCounter}>Number of counter</button>
-      {counterLoop()}
+			<section className="layout">
+				<button onClick={handleCounter}>Number of counter</button>
+				<h1>{counterLoop()}</h1>
+				
+			</section>
+
+			
 		</>
 	);
 }
